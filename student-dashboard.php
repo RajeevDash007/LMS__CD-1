5f43c4c8b9f31fc15001d8776648c1548c05a940
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-08-13 21:04:21 +0530

    updated the js

--- student-dashboard.php
+++ student-dashboard.php
@@ -193,83 +193,8 @@
             sliderFunction.scrollLeft = currentIndex * cardWidth;
         }
     });
-
-    const cards = document.querySelectorAll('.cardc');
-    let card;
-    const modal = document.querySelector('.modal');
-    const closeButton = document.querySelector('.modal__close-button');
-    const page = document.querySelector('.page');
-    const cardBorderRadius = 20;
-    const openingDuration = 600; //ms
-    const closingDuration = 600; //ms
-    const timingFunction = 'cubic-bezier(.76,.01,.65,1.38)';
-
-    function flipAnimation(first, last, options) {
-        let firstRect = first.getBoundingClientRect();
-        let lastRect = last.getBoundingClientRect();
-
-        let deltas = {
-            top: firstRect.top - lastRect.top,
-            left: firstRect.left - lastRect.left,
-            width: firstRect.width / lastRect.width,
-            height: firstRect.height / lastRect.height
-        };
-
-        return last.animate([{
-            transformOrigin: 'top left',
-            borderRadius: `${cardBorderRadius / deltas.width}px / ${cardBorderRadius / deltas.height}px`,
-            transform: `
-                translate(${deltas.left}px, ${deltas.top}px) 
-                scale(${deltas.width}, ${deltas.height})
-            `
-        },{
-            transformOrigin: 'top left',
-            transform: 'none',
-            borderRadius: `${cardBorderRadius}px`
-        }], options);
-    }
-
-    cards.forEach((item)=>{
-        item.addEventListener('click', (e)=>{
-            card = e.currentTarget;
-            page.dataset.modalState = 'opening';
-            card.classList.add('card--opened');
-            card.style.opacity = 0;
-            document.querySelector('body').classList.add('no-scroll');
-
-            let animation = flipAnimation(card, modal, {
-                duration: openingDuration,
-                easing: timingFunction,
-                fill: 'both'
-            });
-
-            animation.onfinish = ()=>{
-                page.dataset.modalState = 'open';
-                animation.cancel();
-            };
-        });
-    });
-
-    closeButton.addEventListener('click', (e)=>{
-        page.dataset.modalState = 'closing';
-        document.querySelector('body').classList.remove('no-scroll');
-
-        let animation = flipAnimation(card, modal, {
-            duration: closingDuration,
-            easing: timingFunction,
-            direction: 'reverse',
-            fill: 'both'
-        });
-
-        animation.onfinish = ()=>{
-            page.dataset.modalState = 'closed';
-            card.style.opacity = 1;
-            card.classList.remove('card--opened');
-            animation.cancel();
-        };
-    });
-});
-</script>                                
+});  
+</script>
 
 
                                     </div><!-- Fim Grids -->
