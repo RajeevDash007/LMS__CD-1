d0eb74036398b04c3bb457e846c8f06b0da3a6d7
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-09-02 09:58:51 +0530

    added statistics section

--- instructor-dashboard.php
+++ instructor-dashboard.php
@@ -60,9 +60,9 @@
                 <a class="d-flex align-items-center border-bottom p-3 text-secondary home active"><i class="lni lni-home size-sm pr-4 font-24"></i>Home</a>
                 <a class="d-flex align-items-center border-bottom p-3 text-secondary acad" id="acad"><i class="lni lni-graduation size-sm pr-4 font-24"></i></i>Assign tasks</a>
                 <a class="d-flex align-items-center border-bottom p-3 text-secondary adm" id="adm"><i class="lni lni-briefcase size-sm pr-4 font-24"></i>Time Table</a>
-                <!-- <a class="d-flex align-items-center border-bottom p-3 text-secondary beneficios"><i
+                <a class="d-flex align-items-center border-bottom p-3 text-secondary beneficios"><i
                         class="lni lni-handshake size-sm pr-4 font-24"></i>Statistics</a>
-                <a class="d-flex align-items-center border-bottom p-3 text-secondary visoes"
+                <!-- <a class="d-flex align-items-center border-bottom p-3 text-secondary visoes"
                     style="white-space: nowrap;"><i class="lni lni-files size-sm pr-4 font-24"></i>extra sect</a> -->
             </div>
         </div>
@@ -203,7 +203,14 @@
                             </div>
                         </div>            
 
-
+                        <div class="beneficios display fadeInUp" style="display: none">
+                            <div class="container">
+                                <div class="mb-5">
+                                    <h3 class="my-4">Statistics</h3>
+                                    <!-- add your code here -->
+                                </div>
+                            </div>
+                        </div>                
 
 
 
