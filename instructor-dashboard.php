7155ba1708f7660532d8458206a4ddc3983327f6
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-09-02 09:44:46 +0530

    added side bar/ navbar

--- instructor-dashboard.php
+++ instructor-dashboard.php
@@ -14,6 +14,33 @@
     <title>Instructor Dashboard</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg align-items-start">
+        <button class="navbar-toggler" type="button" id="menu-toggle">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="col-4 col-sm-3">
+            <img src="https://www.logolynx.com/images/logolynx/0d/0d423bda04a7a684609b06cd46bb699b.png" class="img-fluid logo ml-sm-4" style="opacity: .5;width:100px;" />
+        </div>
+
+        <div class="col-4 col-md-5 d-none d-md-flex  flex-column">
+            <div class="input-group m-2 d-none d-md-flex">
+                <input type="search" class="form-control animated-search-filter search" id="pesquisageral" name="pesquisageral" placeholder="search infos and modules" aria-label="Pesquise" aria-describedby="button-addon2">
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mt-md-2">
+                    <li class="nav-item pl-4 dropdown">
+                        <a class="nav-link dropdown-toggle" style="white-space: normal" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="lni lni-user border rounded-circle border-primary p-1 mr-1"></i> Options<span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     
 </body>
 </html>
