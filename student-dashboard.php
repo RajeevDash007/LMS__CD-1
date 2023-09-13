0b54f6cffc909fa890fbb731b6ef084b416ae1de
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-09-13 21:19:57 +0530

    added swiper js

--- student-dashboard.php
+++ student-dashboard.php
@@ -14,6 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10.2.0/swiper-bundle.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10.2.0/swiper-bundle.min.js"></script>
 
     <title>Student Dashboard</title>
 </head>
