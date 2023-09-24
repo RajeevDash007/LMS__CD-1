9a5de7d8ce28c053f57b676e120e43e3bc2b5d1e
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-09-24 09:37:05 +0530

    added code to check the file type and file handling

--- assets/create_assignment.php
+++ assets/create_assignment.php
@@ -13,6 +13,47 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $semester = $_POST['semester'];
     $course = $_POST['course_name'];
 
+ 
+    $targetDirectory = "uploads/";  
+    $targetFileName = $targetDirectory . basename($_FILES["question_file"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFileName, PATHINFO_EXTENSION));
+
+   
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["question_file"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+   
+    if ($_FILES["question_file"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+ 
+    if ($imageFileType !== "pdf" && $imageFileType !== "docx") {
+        echo "Sorry, only PDF and DOCX files are allowed.";
+        $uploadOk = 0;
+    }
+
+
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["question_file"]["tmp_name"], $targetFileName)) {
+            echo "The file " . htmlspecialchars(basename($_FILES["question_file"]["name"])) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
     
     $sql = "INSERT INTO assignment (AssignmentTitle, AssignmentQuestionURL, AssignmentDueDate, AssignmentMarks, batch)
             VALUES ('$title', '$targetFileName', '$dueDate', $marks, $semester)";
