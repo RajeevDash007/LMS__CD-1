347f55361a2940cff7bb6dadcb701c9a63636c48
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-09-25 18:12:15 +0530

    added php code to retrieve the list and sorted according to deadline

---
+++ assets/fetch_assignments.php
@@ -0,0 +1,32 @@
+<?php
+include 'config.php'; 
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$instructorId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+
+$sql = "SELECT AssignmentTitle, batch, AssignmentDueDate FROM assignment WHERE instructor_id = $instructorId ORDER BY AssignmentDueDate ASC";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $deadline = strtotime($row['AssignmentDueDate']);
+        $currentTime = time();
+        if ($deadline > $currentTime) {
+            echo '<tr>' .
+                '<td>' . $row['AssignmentTitle'] . '</td>' .
+                '<td>' . $row['batch'] . '</td>' .
+                '<td>' . $row['AssignmentDueDate'] . '</td>' .
+                '</tr>';
+        }
+    }
+} else {
+    echo "<tr><td colspan='3'>No assignments found.</td></tr>";
+}
+
+$conn->close();
+?>
