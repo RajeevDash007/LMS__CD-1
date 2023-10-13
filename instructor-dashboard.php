2573623302ed9de7a59d1181cac075e9481e4d85
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-10-13 17:50:07 +0530

    fixed: Calendar hide

--- instructor-dashboard.php
+++ instructor-dashboard.php
@@ -98,6 +98,7 @@ if ($connection) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
@@ -615,7 +616,6 @@ if ($connection) {
                                 </div>
                             </div>
                         </div>
-                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
                         <script>
                             $(document).ready(function() {
