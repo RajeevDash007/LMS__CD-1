07a29b8a16da459a1ebbf7e8bd994cb580099e50
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-10-18 17:38:15 +0530

    css of fetch button

--- student-dashboard.php
+++ student-dashboard.php
@@ -49,6 +49,40 @@ if ($connection) {
         .fc-today{
             background-color: #E35335 !important;
         }
+        
+
+
+#fetchCoursesButton {
+  background-color: #7199ce;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 35px;
+  line-height: 20px;
+  list-style: none;
+  margin-left: 5px;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: color 100ms;
+  vertical-align: baseline;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+#fetchCoursesButton:hover,
+#fetchCoursesButton:focus {
+  background-color: #7199ce;
+}
     </style>
 </head>
 
