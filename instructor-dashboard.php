bd7889d0dfe0c34f89ed8ee8d856149023372167
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-11-06 17:36:42 +0530

    added code to change the styles of the button

--- instructor-dashboard.php
+++ instructor-dashboard.php
@@ -159,6 +159,50 @@ if ($connection) {
             overflow: hidden;
             text-overflow: ellipsis;
         }
+        button {
+            align-items: center;
+            appearance: none;
+            background-image: radial-gradient(100% 100% at 100% 0, #5adaff 0, #5468ff 100%);
+            border: 0;
+            border-radius: 6px;
+            box-shadow: rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, rgba(58, 65, 111, .5) 0 -3px 0 inset;
+            box-sizing: border-box;
+            color: #fff;
+            cursor: pointer;
+            display: inline-flex;
+            font-family: "JetBrains Mono", monospace;
+            height: 48px;
+            justify-content: center;
+            line-height: 1;
+            list-style: none;
+            overflow: hidden;
+            padding-left: 16px;
+            padding-right: 16px;
+            position: relative;
+            text-align: left;
+            text-decoration: none;
+            transition: box-shadow .15s, transform .15s;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+            white-space: nowrap;
+            will-change: box-shadow, transform;
+            font-size: 18px;
+        }
+
+        button:focus {
+            box-shadow: #3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+        }
+
+        button:hover {
+            box-shadow: rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+            transform: translateY(-2px);
+        }
+
+        button:active {
+            box-shadow: #3c4fe0 0 3px 7px inset;
+            transform: translateY(2px);
+        }
 
         .custom-card .card-text {
             color: #666;
@@ -742,7 +786,7 @@ if ($connection) {
 
                                                         if (mysqli_num_rows($result) > 0) {
                                                             $serialNo = 1;
-                                                            
+
                                                             while ($row = mysqli_fetch_assoc($result)) {
                                                                 echo "<tr>";
                                                                 echo "<td>" . $serialNo++ . "</td>";
@@ -757,7 +801,7 @@ if ($connection) {
                                 </td>';
                                                                 echo "</tr>";
                                                             }
-                                                        } 
+                                                        }
                                                     } else {
                                                         echo "Instructor not logged in.";
                                                     }
